ac049f278c2b3325f0599bfe01985c66d1c9178e
Ben Marx <benyamin.marx@gmail.com>
2014-10-25 20:49:15 +0900

    vocab pagination etc

--- app/controllers/vocabularies_controller.rb
+++ app/controllers/vocabularies_controller.rb
@@ -2,8 +2,7 @@ class VocabulariesController < ApplicationController
   before_action :get_cards, only: :index
 
   def index
-    @card = @cards[rand(@cards.size)]
-    @card
+    @card = vocabulary_card.merge(page_total)
   end
 
   def new
@@ -24,18 +23,26 @@ class VocabulariesController < ApplicationController
 
   def api_get_connection
     response = api_connection.get do |req|
-      req.url "/vocabularies?level=#{params[:level]}&page=2"
+      req.url "/vocabularies?level=#{params[:level]}"
     end
     response
   end
 
+  def vocabulary_card
+    @paginated_cards = @cards["vocabulary"]["vocabulary_card"]
+    @paginated_cards[rand(@paginated_cards.size)]
+  end
+
+  def page_total
+    { total_num_pages: @cards["vocabulary"]["vocabulary_page_total"] }
+  end
+
   def url
-    "http://dry-refuge-1311.herokuapp.com/"
+    "http://localhost:4000/"
+    #"http://dry-refuge-1311.herokuapp.com/"
   end
 
   def get_cards
-    @cards = ActiveSupport::JSON.decode api_get_connection.body
-    @cards = @cards["vocabulary"]
-    @cards
+    @cards ||= ActiveSupport::JSON.decode api_get_connection.body
   end
 end
